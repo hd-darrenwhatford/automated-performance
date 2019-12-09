eaf7780015265482bb01d09fc84040d04fe78a1f
Dan Jackson <danjackson@hastingsdirect.com>
2019-12-09 14:35:08 +0000

    Subdirectories added for the JS and CSS files

---
+++ js/script.js
@@ -0,0 +1,193 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCA_mM9FFLpxGzZrAWoV3ckC7LPqDrmLzg",
+	authDomain: "automated-performance.firebaseapp.com",
+	databaseURL: "https://automated-performance.firebaseio.com",
+	projectId: "automated-performance",
+	storageBucket: "automated-performance.appspot.com",
+	messagingSenderId: "391320435503",
+	appId: "1:391320435503:web:6241d51d37c2f51dce7fda",
+	measurementId: "G-2SCLJLN567"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Get a reference to the database
+var ref = firebase.database().ref('/'); 
+console.log(ref);
+
+var dbVal = document.getElementById("dbVal");
+
+ref.on('value', function(snapshot) {
+//ref.limitToLast(4).on('value', function(snapshot) {
+	var value = snapshot.val();
+	console.log(value);
+
+	// Create an empty array to store values from Firebase
+	var data = [];
+
+	// Loop through the data in the database
+	snapshot.forEach(function(childNodes) {
+		// Get each value from each node in the console e.g. Dan, 2
+		console.log(childNodes.val());
+
+		data.push(childNodes.val());
+	});
+
+	console.log(data);
+
+	// Declare a tooltip
+	var tooltip = d3.select("body")
+  		.append("div")
+  		.attr('class', 'tooltip');
+
+	// Create the chart from data passed into the data array
+	// https://bost.ocks.org/mike/bar/
+	var chart = d3.select(".chart")
+		.selectAll("div")
+		.data(data)
+		.enter()
+		.append("div")
+		//.style("width", function(d) { return d + "px"; }).text(function(d) { return d; });
+		// Set the width of each new bar as a multiple of the associated data value, d
+		//.style("width", function(d) { return d * 10 + "px"; })
+		.style("width", function(d) { return d + "%"; })
+		// Set the text content of each bar and produce a label
+		.text(function(d) { return d; });	
+
+  		// we define "mouseover" handler, here we change tooltip
+  		// visibility to "visible" and add appropriate test
+  		chart.on("mouseover", function(d) {
+  			// This will return the value of the bar
+    		return tooltip.style("visibility", "visible").text(d);
+  		})
+  
+  		// we move tooltip during of "mousemove"
+  		chart.on("mousemove", function() {
+    		return tooltip.style("top", (event.pageY - 30) + "px")
+      		.style("left", event.pageX + "px");
+  		})
+  
+  		// we hide our tooltip on "mouseout"
+  		chart.on("mouseout", function() {
+    		return tooltip.style("visibility", "hidden");
+  		});
+
+	// Create the chart from data passed into the data array
+	// https://bost.ocks.org/mike/bar/
+	var chartTwo = d3.select(".chart-two")
+		.selectAll("div")
+		.data(data)
+		.enter()
+		.append("div")
+		//.style("width", function(d) { return d + "px"; }).text(function(d) { return d; });
+		// Set the width of each new bar as a multiple of the associated data value, d
+		//.style("width", function(d) { return d * 10 + "px"; })
+		.style("width", function(d) { return d + "%"; })
+		// Set the text content of each bar and produce a label
+		.text(function(d) { return d; });	
+
+  		// we define "mouseover" handler, here we change tooltip
+  		// visibility to "visible" and add appropriate test
+  		chartTwo.on("mouseover", function(d) {
+  			// This will return the value of the bar
+    		return tooltip.style("visibility", "visible").text(d);
+  		})
+  
+  		// we move tooltip during of "mousemove"
+  		chartTwo.on("mousemove", function() {
+    		return tooltip.style("top", (event.pageY - 30) + "px")
+      		.style("left", event.pageX + "px");
+  		})
+  
+  		// we hide our tooltip on "mouseout"
+  		chartTwo.on("mouseout", function() {
+    		return tooltip.style("visibility", "hidden");
+  		});
+
+	// Create the chart from data passed into the data array
+	// https://bost.ocks.org/mike/bar/
+	var chartThree = d3.select(".chart-three")
+		.selectAll("div")
+		.data(data)
+		.enter()
+		.append("div")
+		//.style("width", function(d) { return d + "px"; }).text(function(d) { return d; });
+		// Set the width of each new bar as a multiple of the associated data value, d
+		//.style("width", function(d) { return d * 10 + "px"; })
+		.style("width", function(d) { return d + "%"; })
+		// Set the text content of each bar and produce a label
+		.text(function(d) { return d; });	
+
+  		// we define "mouseover" handler, here we change tooltip
+  		// visibility to "visible" and add appropriate test
+  		chartThree.on("mouseover", function(d) {
+  			// This will return the value of the bar
+    		return tooltip.style("visibility", "visible").text(d);
+  		})
+  
+  		// we move tooltip during of "mousemove"
+  		chartThree.on("mousemove", function() {
+    		return tooltip.style("top", (event.pageY - 30) + "px")
+      		.style("left", event.pageX + "px");
+  		})
+  
+  		// we hide our tooltip on "mouseout"
+  		chartThree.on("mouseout", function() {
+    		return tooltip.style("visibility", "hidden");
+  		});
+});
+
+	/*
+	// On any "value event" pass a snapshot of the database to the callback function                          
+	ref.on('value', function(snapshot) {
+		// Console log the snapshot of the database
+		console.log(snapshot.val());
+		// Stringify the JSON document and output the data to the HTML document
+		output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
+
+		// Loop through the data in the database
+		snapshot.forEach(function(childNodes) {
+			// Output the value in the console
+			console.log(childNodes.val());
+			// Output the value onto the page
+			dbVal.innerHTML = childNodes.val();
+
+			// This outputs the 'test3' database value
+			var performanceVal = childNodes.val().test3;	// 2
+			console.log(performanceVal);
+
+			//d3.select("body").style("background-color", "cyan");
+			var data = [30, 6, 168, 281, 303, 365];
+
+			data.push(performanceVal);
+
+			// https://www.dashingd3js.com/using-json-to-simplify-code
+			d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) { return d + "px"; }).text(function(d) { return d; });
+		});
+	});
+	*/
+
+	// Add a value to the database but this overwrites what has gone before
+	/*
+	ref.set({
+		"test2": "Dan"
+	});
+	*/
+
+	// Add additional value to the database using the update method
+	/*
+	ref.update({
+		"test3": 2
+	});
+	*/
+
+	/*
+//d3.select("body").style("background-color", "cyan");
+var data = [30, 6, 168, 281, 303, 365];
+
+// https://www.dashingd3js.com/using-json-to-simplify-code
+d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) { return d + "px"; }).text(function(d) { return d; });
+*/
